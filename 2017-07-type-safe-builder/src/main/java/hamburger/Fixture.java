b4fa051a5e482beaa75e16a9a467e51ef8295f2f
suzuki-hoge <user.ryo@gmail.com>
2020-01-18 23:37:54 +0900

    implement origin code.

---
+++ 2017-07-type-safe-builder/src/main/java/hamburger/Fixture.java
@@ -0,0 +1,6 @@
+package hamburger;
+
+class Fixture {
+    public static final String てりやき_オレンジ = "{ 'main': 'てりやき', 'drink': 'オレンジ', 'options': [], 'sides': [] }";
+    public static final String チーズ_コーヒー_ミルク_ポテト_ナゲット = "{ 'main': 'チーズ', 'drink': 'コーヒー', 'options': ['ミルク'], 'sides': ['ポテト', 'ナゲット'] }";
+}
